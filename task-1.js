ad6014c7e1cbf556595a1ce728b7ad823727fef9
Oleksandr Slipchenko <o.p.slipchenko@gmail.com>
2020-10-10 19:06:16 +0300

    Подправил код

--- task-1.js
+++ task-1.js
@@ -10,18 +10,22 @@ const openModalRef = document.querySelector('.js-lightbox');
 
 const closeBtnModalRef = document.querySelector('[data-action="close-lightbox"]');
 
+const imgGalleryRef = document.querySelector('.gallery__image');
+
 const imgModalRef = document.querySelector('.lightbox__image');
 
 
 //               Functions
 
 function createListPreviewGalleryMarkup(items) {
-  return items.map(item => `<li class="gallery__item"><a class="gallery__link"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></li>`).join('');
+  return items.map(item => `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></li>`).join('');
 }
 
 const listItemsMarkup = createListPreviewGalleryMarkup(galleryItems);
 
 galleryRef.innerHTML = listItemsMarkup;
+
+// console.log(listItemsMarkup);
 
 function onOpenModalClick(e) {
   e.preventDefault();
@@ -30,10 +34,9 @@ function onOpenModalClick(e) {
     return;
   }
   openModalRef.classList.add('is-open');
-// console.log(e.target);
+
   imgModalRef.src = e.target.dataset.source;
   imgModalRef.alt = e.target.alt;
-console.log(imgModalRef.alt);
 }
 
 function onCloseModalClick(e) {
@@ -57,12 +60,11 @@ function onKeyEscCloseModal(e) {
   }
 }
 
-//               EventListener
+
+//               EventListeners
 
 galleryRef.addEventListener('click', onOpenModalClick);
 
 closeBtnModalRef.addEventListener('click', onCloseModalClick);
 
 modalWindowRef.addEventListener('click', onBackdropClose);
-
-// window.addEventListener('keydown', onChangePhoto);
